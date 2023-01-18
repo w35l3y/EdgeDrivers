c3745e1b340d42b5e7df9a1a9333c316835cde3d
w35l3y <w35l3y@brasnet.org>
2023-01-18 09:24:33 -0300

    feat: created multiple packageKey

--- personal-tuya-devices/src/sub_drivers/info/init.lua
+++ personal-tuya-devices/src/sub_drivers/info/init.lua
@@ -1,5 +1,5 @@
---local log = require "log"
---local utils = require "st.utils"
+-- local log = require "log"
+-- local utils = require "st.utils"
 
 local capabilities = require "st.capabilities"
 
@@ -18,5 +18,12 @@ return {
     added = function (driver, device, ...)
       device:emit_event(info.value(tostring(myutils.info(device))))
     end,
+  },
+  capability_handlers = {
+    [info.ID] = {
+      [info.commands.clear.NAME] = function (driver, device, ...)
+        device:emit_event(info.value(tostring(myutils.info(device)), { visibility = { displayed = false } }))
+      end
+    },
   },
 }
