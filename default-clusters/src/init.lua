bd32afef736a72f4fa48af1dd6fcbf0beff970b9
w35l3y <w35l3y@brasnet.org>
2023-01-21 20:30:09 -0300

    fix: test

--- default-clusters/src/init.lua
+++ default-clusters/src/init.lua
@@ -1,3 +1,5 @@
+local log = require "log"
+
 local data_types = require "st.zigbee.data_types"
 local zb_const = require "st.zigbee.constants"
 local zcl_clusters = require "st.zigbee.zcl.clusters"
@@ -71,9 +73,10 @@ local template = {
       if device.network_type == device_lib.NETWORK_TYPE_ZIGBEE then
         device:set_find_child(utils.find_child_fn)
 
-        -- utils.spell_magic_trick(device)
-
-        device:emit_event(capabilities["valleyboard16460.info"].value(tostring(utils.info(device))))
+        driver:call_with_delay(0, function ()
+          utils.spell_magic_trick(device)
+          device:emit_event(capabilities["valleyboard16460.info"].value(tostring(utils.info(device))))
+        end)
       end
     end,
     added = function (driver, device, ...)
--- default-clusters/src/test/switch.lua
+++ default-clusters/src/test/switch.lua
@@ -1,42 +1,14 @@
 local test = require "integration_test"
-local clusters = require "st.zigbee.zcl.clusters"
-local OnOff = clusters.OnOff
 local capabilities = require "st.capabilities"
 local data_types = require "st.zigbee.data_types"
 local zcl_clusters = require "st.zigbee.zcl.clusters"
 local zigbee_test_utils = require "integration_test.zigbee_test_utils"
 local t_utils = require "integration_test.utils"
 
-local cluster_base = require "st.zigbee.cluster_base"
-local read_attribute = require "st.zigbee.zcl.global_commands.read_attribute"
-local zcl_messages = require "st.zigbee.zcl"
-local messages = require "st.zigbee.messages"
-local zb_const = require "st.zigbee.constants"
-
-function cluster_base.build_test_read_attributes(device, cluster_id, attr_ids)
-  local read_body = read_attribute.ReadAttribute(attr_ids)
-  local zclh = zcl_messages.ZclHeader({
-    cmd = data_types.ZCLCommandId(read_attribute.ReadAttribute.ID)
-  })
-  local addrh = messages.AddressHeader(
-      zb_const.HUB.ADDR,
-      zb_const.HUB.ENDPOINT,
-      device:get_short_address(),
-      device:get_endpoint(cluster_id.value),
-      zb_const.HA_PROFILE_ID,
-      cluster_id.value
-  )
-  local message_body = zcl_messages.ZclMessageBody({
-    zcl_header = zclh,
-    zcl_body = read_body
-  })
-  return messages.ZigbeeMessageTx({
-    address_header = addrh,
-    body = message_body
-  })
-end
-
 local profile = t_utils.get_profile_definition("switch_v1.yaml")
+
+test.load_all_caps_from_profile(profile)
+
 local mock_parent_device = test.mock_device.build_test_zigbee_device(
     {
       profile = profile,
@@ -45,7 +17,8 @@ local mock_parent_device = test.mock_device.build_test_zigbee_device(
           id = 1,
           manufacturer = "switch",
           model = "switch",
-          server_clusters = { 0x0000, 0x0006 }
+          server_clusters = { 0x0000, 0x0006 },
+					client_clusters = { }
         },
       },
       fingerprinted_endpoint_id = 0x01
@@ -63,29 +36,41 @@ test.register_message_test(
   "Reported on off status should be handled: on ep 1",
   {
     -- {
-    --   channel = "zigbee",
+    --   channel = "device_lifecycle",
+    --   direction = "receive",
+    --   message = { mock_parent_device.id, "init" }
+    -- },
+    -- {
+    --  channel = "zigbee",
+    --  direction = "send",
+    --  message = { mock_parent_device.id, zigbee_test_utils.build_attribute_read(mock_parent_device, zcl_clusters.Basic.ID, {
+    --    data_types.AttributeId(zcl_clusters.Basic.attributes.ManufacturerName.ID),
+    --    data_types.AttributeId(zcl_clusters.Basic.attributes.ZCLVersion.ID),
+    --    data_types.AttributeId(zcl_clusters.Basic.attributes.ApplicationVersion.ID),
+    --    data_types.AttributeId(zcl_clusters.Basic.attributes.ModelIdentifier.ID),
+    --    data_types.AttributeId(zcl_clusters.Basic.attributes.PowerSource.ID),
+    --    data_types.AttributeId(0xFFFE),
+    --  }):to_endpoint(0x01) }
+    -- },
+    -- {
+    --   channel = "capability",
     --   direction = "send",
-    --   message = { mock_parent_device.id, cluster_base.build_test_read_attributes(mock_parent_device, data_types.ClusterId(zcl_clusters.Basic.ID), {
-    --     data_types.AttributeId(zcl_clusters.Basic.attributes.ManufacturerName.ID),
-    --     data_types.AttributeId(zcl_clusters.Basic.attributes.ZCLVersion.ID),
-    --     data_types.AttributeId(zcl_clusters.Basic.attributes.ApplicationVersion.ID),
-    --     data_types.AttributeId(zcl_clusters.Basic.attributes.ModelIdentifier.ID),
-    --     data_types.AttributeId(zcl_clusters.Basic.attributes.PowerSource.ID),
-    --     data_types.AttributeId(0xFFFE),
-    --   }):to_endpoint(0x01) }
+    --   message = mock_parent_device:generate_test_message("main", capabilities["valleyboard16460.info"].value("A"))
     -- },
     {
       channel = "zigbee",
       direction = "receive",
-      message = { mock_parent_device.id, OnOff.attributes.OnOff:build_test_attr_report(mock_parent_device,
+      message = { mock_parent_device.id, zcl_clusters.OnOff.attributes.OnOff:build_test_attr_report(mock_parent_device,
           true):from_endpoint(0x01) }
     },
     {
       channel = "capability",
       direction = "send",
       message = mock_parent_device:generate_test_message("main",  capabilities.switch.switch.on())
-    }
-  }
+    },
+  }, {
+		-- test_init=function() end,
+	}
 )
 
 test.run_registered_tests()
