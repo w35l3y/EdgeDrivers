04869e39c50f8e0b33c5dc4be8ed632bc68f0d24
w35l3y <w35l3y@brasnet.org>
2023-01-20 23:48:36 -0300

    fix: tests
    commented out publish temporarily

--- default-clusters/src/test/switch.lua
+++ default-clusters/src/test/switch.lua
@@ -5,6 +5,35 @@ local capabilities = require "st.capabilities"
 local zigbee_test_utils = require "integration_test.zigbee_test_utils"
 local t_utils = require "integration_test.utils"
 
+local cluster_base = require "st.zigbee.cluster_base"
+local read_attribute = require "st.zigbee.zcl.global_commands.read_attribute"
+local zcl_messages = require "st.zigbee.zcl"
+local messages = require "st.zigbee.messages"
+local zb_const = require "st.zigbee.constants"
+
+function cluster_base.build_test_read_attributes(device, cluster_id, attr_ids)
+  local read_body = read_attribute.ReadAttribute(attr_ids)
+  local zclh = zcl_messages.ZclHeader({
+    cmd = data_types.ZCLCommandId(read_attribute.ReadAttribute.ID)
+  })
+  local addrh = messages.AddressHeader(
+      zb_const.HUB.ADDR,
+      zb_const.HUB.ENDPOINT,
+      device:get_short_address(),
+      device:get_endpoint(cluster_id.value),
+      zb_const.HA_PROFILE_ID,
+      cluster_id.value
+  )
+  local message_body = zcl_messages.ZclMessageBody({
+    zcl_header = zclh,
+    zcl_body = read_body
+  })
+  return messages.ZigbeeMessageTx({
+    address_header = addrh,
+    body = message_body
+  })
+end
+
 local profile = t_utils.get_profile_definition("switch_v1.yaml")
 local mock_parent_device = test.mock_device.build_test_zigbee_device(
     {
@@ -12,8 +41,8 @@ local mock_parent_device = test.mock_device.build_test_zigbee_device(
       zigbee_endpoints = {
         [1] = {
           id = 1,
-          manufacturer = "single_switch",
-          model = "single_switch",
+          manufacturer = "switch",
+          model = "switch",
           server_clusters = { 0x0000, 0x0006 }
         },
       },
@@ -30,6 +59,11 @@ end)
 test.register_message_test(
     "Reported on off status should be handled: on ep 1",
     {
+      {
+        channel = "zigbee",
+        direction = "send",
+        message = { mock_parent_device.id, cluster_base.build_test_read_attributes(mock_parent_device, 0, {4,0,1,5,7,0xFFFE}):to_endpoint(0x01) }
+      },
       {
         channel = "zigbee",
         direction = "receive",
