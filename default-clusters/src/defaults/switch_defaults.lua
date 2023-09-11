ad7b97bdb9abec885ac8f9126cb13092a3141eee
w35l3y <w35l3y@brasnet.org>
2023-09-10 21:09:18 -0300

    feat: added button profile

--- default-clusters/src/defaults/switch_defaults.lua
+++ default-clusters/src/defaults/switch_defaults.lua
@@ -1,5 +1,11 @@
+local log = require "log"
+
 local zcl_clusters = require "st.zigbee.zcl.clusters"
 local capabilities = require "st.capabilities"
+
+local base_device = require "st.device"
+
+local utils = require "utils"
 
 local OnOff = zcl_clusters.OnOff
 
@@ -39,6 +45,17 @@ local switch_defaults = require "st.zigbee.defaults.switch_defaults"
 --   end
 -- end
 
+function get_profile(device, endpoint)
+  local find_child_fn = device:get_field(base_device.FIND_CHILD_KEY)
+  if find_child_fn ~= nil then
+    local child = find_child_fn(device, endpoint)
+    if child ~= nil then
+      return child.preferences.profile,child
+    end
+  end
+  return device.preferences.profile,device
+end
+
 function switch_defaults.button_pressed_handler(driver, device, zb_rx)
   local button_status = zb_rx.body.zcl_body.button_status.value
   local ButtonStatus = require "st.zigbee.generated.zcl_clusters.OnOff.types.ButtonStatus"
@@ -49,7 +66,18 @@ function switch_defaults.button_pressed_handler(driver, device, zb_rx)
   }
   local event = STATUS[button_status]
   if (event ~= nil) then
-    device:emit_event_for_endpoint(zb_rx.address_header.src_endpoint.value, event)
+    local endpoint = zb_rx.address_header.src_endpoint.value
+    local profile, dev = get_profile(device, endpoint)
+  
+    if profile == "child_switch_v1" then
+      utils.update_profile(dev, "child_button_v1")
+    elseif profile == "switch_v1" then
+      utils.update_profile(dev, "button_v1")
+    end
+
+    device:emit_event_for_endpoint(endpoint, event)
+  elseif button_status then
+    log.warn("Unknown button status found", button_status)
   end
 end
 
