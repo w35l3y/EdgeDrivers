a6fa8d0294190ea2e69ab0316c1544c489181a22
w35l3y <w35l3y@brasnet.org>
2023-12-04 08:56:48 -0300

    fix: missed dependency

--- personal-tuya-devices/src/st/zigbee/generated/zcl_clusters/TuyaEF00/types/IntABC.lua
+++ personal-tuya-devices/src/st/zigbee/generated/zcl_clusters/TuyaEF00/types/IntABC.lua
@@ -11,6 +11,7 @@
 -- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 -- See the License for the specific language governing permissions and
 -- limitations under the License.
+local zb_utils = require "st.zigbee.utils"
 local st_utils = require "st.utils"
 
 --- @class st.zigbee.data_types.IntABC: st.zigbee.data_types.DataType
