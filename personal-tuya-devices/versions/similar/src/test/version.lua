c4183a8b3c7654de22c7a555f57f1c0087da03d6
Konnichy <katakayann@gmail.com>
2024-12-28 06:00:00 +0100

    FIX

--- personal-tuya-devices/versions/similar/src/test/version.lua
+++ personal-tuya-devices/versions/similar/src/test/version.lua
@@ -17,7 +17,6 @@ require "test.normal-temphumi-v1"
 require "test.normal-temphumibatt-v1"
 require "test.normal-presenceSensor-v1"
 require "test.normal-garageDoor-v1"
-require "test.normal-single-powerMeter-v1"
 require "test.normal-irrigation-v1"
 require "test.normal-irrigation-v2"
 
